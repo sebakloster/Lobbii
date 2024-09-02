e3cb4e7bebeb87bb3f08eff2f20d2ac02d34dbde
Seba Kloster <sebakloster@gmail.com>
2024-09-02 18:02:35 -0400

    [new build] Production build

--- backend/config.js
+++ backend/config.js
@@ -2,12 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = {
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-  // productionDatabaseUrl: process.env.PRODUCTION_DATABASE_URL,
+  // database: process.env.DB_NAME,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // host: process.env.DB_HOST,
+  // dialect: process.env.DB_DIALECT,
+  productionDatabaseUrl: process.env.PRODUCTION_DATABASE_URL,
   // logging: (...msg) => console.log(msg),
   logging: false,
 };
